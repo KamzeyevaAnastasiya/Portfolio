ae37687c846ad8e2fcddc29139cfab0d8e1dee07
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-10 23:28:56 +0300

    Add Container.ts and add container to header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,23 +1,29 @@
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
+import { Container } from "../../components/Container";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 const items = ["Projects", "About", "Digital Assets"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
-            <Button>Let’s Talk</Button>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <FlexWrapper justify={"space-around"} align={"center"}>
+                        <Menu menuItems={items}/>
+                        <Button>Let’s Talk</Button>
+                    </FlexWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
     background-color: #b8f3b7;
-    display: flex;
-    justify-content: space-between;
 `
 
 const Button = styled.button`
@@ -26,4 +32,5 @@ const Button = styled.button`
     font-size: 16px;
     line-height: 1.5;
     color: #fff;
+    margin-left: 32px;
 `
