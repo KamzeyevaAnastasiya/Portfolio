906247a0fbb5fd975df9f2a0ec5b35d34c1739bd
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-11 17:09:54 +0300

    Add mainFn

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -3,8 +3,9 @@ export const Theme = {
         primaryBg: "#ffffff",
         secondaryBg: "#131212",
         
+        mainFn: "#ffffff",
         primaryFn: "#2D2C2C",
         secondaryFn: "#000000",
-        h1Fn: "#0D0F16"
+        h1Fn: "#0D0F16",
     }
 }
