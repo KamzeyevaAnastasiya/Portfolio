e1acf1406da03706c02f68a0c0519ff5f48e312a
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-11 17:18:22 +0300

    Style header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
-import { Menu } from "../../components/menu/Menu";
+import { HeaderMenu } from "../../layout/header/headerMenu/HeaderMenu";
 import { Container } from "../../components/Container";
 import { FlexWrapper } from "../../components/FlexWrapper";
+import { Theme } from "../../styles/Theme";
 
 const items = ["Projects", "About", "Digital Assets"]
 
@@ -12,8 +13,8 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
-                    <FlexWrapper justify={"space-around"} align={"center"}>
-                        <Menu menuItems={items}/>
+                    <FlexWrapper align={"center"}>
+                        <HeaderMenu menuItems={items}/>
                         <Button>Let’s Talk</Button>
                     </FlexWrapper>
                 </FlexWrapper>
@@ -23,14 +24,24 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-    background-color: #b8f3b7;
+    background-color: ${Theme.colors.primaryBg};
+    padding: 20px 200px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0%;
+    z-index: 99999;
 `
 
 const Button = styled.button`
-    background-color: #2d2c2c;
+    background-color: ${Theme.colors.primaryFn};
+    padding: 8px 20px;
+    width: 111px;
+    height: 40px;
+    font-family: "Epilogue", sans-serif;
     font-weight: 400;
     font-size: 16px;
     line-height: 1.5;
-    color: #fff;
+    color: ${Theme.colors.mainFn};
     margin-left: 32px;
 `
