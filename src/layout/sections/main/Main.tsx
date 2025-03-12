71b1aeb7ff9fe0d9b4855503fd50b4d6e9d4b0e3
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-13 00:06:24 +0300

    Refactoring

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,32 +2,35 @@ import styled from 'styled-components';
 import photo from '../../../assets/images/myPhoto.webp'
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { SectionTitle } from '../../../components/SectionTitle'
+import { Container } from '../../../components/Container';
+import { MainTitle } from '../../../components/h1/H1';
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <SectionTitle>HELLO I’M ANASTASIYA</SectionTitle>
-                    <MainTitle>FRONT-END DEVELOPER</MainTitle>
-                </div>
-
-                <Photo src={photo} alt="" />
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-around"}>
+                    <FlexWrapper direction={"column"} justify={"center"} align={"left"}>
+                        <SectionTitle>HELLO <br/> I’M ANASTASIYA</SectionTitle>
+                        <MainTitle/>
+                    </FlexWrapper>
+                    <Photo src={photo} alt="My photo" />
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
 
 const StyledMain = styled.section`
     min-height: 100vh;
+    display: flex;
+    padding: 200px 105px 105px 175px;
 `
 
 const Photo = styled.img`
     width: 570px;
     height: 570px;
     object-fit: cover;
-`
-
-const MainTitle = styled.h1`
-
+    transform: rotate(8deg);
+    opacity: 0.9;
 `
