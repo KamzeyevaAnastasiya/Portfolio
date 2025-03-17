b2b33d0fa678713f32b8d299cd6d2a32bf552b46
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-17 15:47:14 +0300

    Delet

--- src/components/SectionTitle.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { Theme } from "../styles/Theme";
-
-export const SectionTitle = styled.h2`
-    font-family: "Epilogue", sans-serif;
-    font-weight: 800;
-    font-size: 70px;
-    color: ${Theme.colors.primaryFn};
-`
