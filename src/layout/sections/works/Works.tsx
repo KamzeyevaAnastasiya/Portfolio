dcac754d4b071b40cf262eb36f19e7b35af8eba3
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-18 21:15:04 +0300

    Add container

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -5,27 +5,30 @@ import healthImg from '../../../assets/images/proj-1.webp'
 import fashionImg from '../../../assets/images/proj-2.webp'
 import barbequeImg from '../../../assets/images/proj-3.webp'
 import { Theme } from "../../../styles/Theme"
+import { Container } from "../../../components/Container"
 
 export const Works = () => {
     return (
         <StyledWorks>
-            <WorksTitle>MY PROJECTS</WorksTitle>
-            <FlexWrapper direction={"column"}>
-                <Work src={healthImg} 
-                category={"Mental Health"} 
-                title={"A Case Study_Find Peace in Meditation"} 
-                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."}/>
-                
-                <Work src={fashionImg} 
-                category={"Fashion Show"} 
-                title={"A Case Study_Enjoy Fashion Shows From Home"} 
-                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."}/>
-                
-                <Work src={barbequeImg} 
-                category={"Barbeque"} 
-                title={"Branding_Chicken Barbeque Never Goes Wrong"} 
-                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."}/>
-            </FlexWrapper>
+            <Container>
+                <WorksTitle>MY PROJECTS</WorksTitle>
+                <FlexWrapper direction={"column"}>
+                    <Work src={healthImg} 
+                    category={"Mental Health"} 
+                    title={"A Case Study_Find Peace in Meditation"} 
+                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."}/>
+                    
+                    <Work src={fashionImg} 
+                    category={"Fashion Show"} 
+                    title={"A Case Study_Enjoy Fashion Shows From Home"} 
+                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."}/>
+                    
+                    <Work src={barbequeImg} 
+                    category={"Barbeque"} 
+                    title={"Branding_Chicken Barbeque Never Goes Wrong"} 
+                    text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."}/>
+                </FlexWrapper>
+            </Container>
         </StyledWorks>
     );
 };
