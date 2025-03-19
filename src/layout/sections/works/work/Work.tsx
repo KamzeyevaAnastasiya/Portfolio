7897e5855dbd44da286bd6cc9b51f0ed82f890ac
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-19 19:26:03 +0300

    Add TitleWrapper and TextWrapper

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -16,17 +16,25 @@ export const Work = (props: WorkPropsType) => {
             <ImageWrapper>
                 <Image src={props.src} alt=""/>
             </ImageWrapper>
-            <Category>{props.category}</Category>
-            <TitlePart1>{props.titlePart1}</TitlePart1>
-            <TitlePart2>{props.titlePart2}</TitlePart2>
-            <Text>{props.text}</Text>
-            <StyledButton/>
+            
+            <TitleWrapper>
+                <Category>{props.category}</Category>
+                <TitlePart1>{props.titlePart1}</TitlePart1>
+                <TitlePart2>{props.titlePart2}</TitlePart2>
+            </TitleWrapper>
+            
+            <TextWrapper>
+                <Text>{props.text}</Text>
+                <StyledButton/>
+            </TextWrapper>
+            
         </StyledWork>
     );
 };
 
 const StyledWork = styled.div`
     margin-top: 80px;
+    position: relative;
 `
 
 const ImageWrapper = styled.div`
@@ -50,12 +58,22 @@ const Image = styled.img`
     object-fit: cover;
 `
 
+const TitleWrapper = styled.div`
+    width: 433px;
+    min-height: 202px;
+    
+    position: absolute;
+    left: 45px;
+    top: 90px;
+`
+
 const Category = styled.h5`
     font-family: "Epilogue", sans-serif;
     font-weight: 600;
     font-size: 16px;
     line-height: 1.5;
     color: ${Theme.colors.mainFn};
+    margin-bottom: 16px;
 `
 
 const TitlePart1 = styled.h3`
@@ -72,11 +90,21 @@ const TitlePart2 = styled.h3`
     color: ${Theme.colors.mainFn};
 `
 
+const TextWrapper = styled.div`
+    width: 433px;
+    min-height: 215px;
+    
+    position: absolute;
+    right: 45px;
+    top: 130px;
+`
+
 const Text = styled.p`
     font-family: "Epilogue", sans-serif;
     font-weight: 400;
     font-size: 18px;
     line-height: 1.5;
     color: ${Theme.colors.mainFn};
+    margin-bottom: 40px;
 `
 
