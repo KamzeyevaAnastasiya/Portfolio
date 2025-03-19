8882f288e372b5b1f1f59db2b51edf537661d9ac
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-19 17:48:16 +0300

    Style and refactoring title

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components"
 import { StyledButton } from "../../../../components/button/Button"
+import { Theme } from "../../../../styles/Theme"
 
 type WorkPropsType = {
     category: string
-    title: string
+    titlePart1: string
+    titlePart2: string
     text: string
     src: string
 }
@@ -13,7 +15,8 @@ export const Work = (props: WorkPropsType) => {
         <StyledWork>
             <Image src={props.src} alt=""/>
             <Category>{props.category}</Category>
-            <Title>{props.title}</Title>
+            <TitlePart1>{props.titlePart1}</TitlePart1>
+            <TitlePart2>{props.titlePart2}</TitlePart2>
             <Text>{props.text}</Text>
             <StyledButton/>
         </StyledWork>
@@ -21,7 +24,7 @@ export const Work = (props: WorkPropsType) => {
 };
 
 const StyledWork = styled.div`
-    
+    margin-top: 80px;
 `
 
 const Image = styled.img`
@@ -30,11 +33,19 @@ const Image = styled.img`
     object-fit: cover;
 `
 
-const Category = styled.div`
+const Category = styled.h5`
+    font-family: "Epilogue", sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${Theme.colors.mainFn};
+`
+
+const TitlePart1 = styled.h3`
     
 `
 
-const Title = styled.h5`
+const TitlePart2 = styled.h3`
     
 `
 
