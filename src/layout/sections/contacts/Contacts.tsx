7f6bcc48e246582a26487df217741c61603da2ff
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-20 17:42:33 +0300

    Style section

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,34 +2,45 @@ import styled from "styled-components"
 import { ContactsMenu } from "./contactsMenu/ContactsMenu"
 import { Logo } from "../../../components/logo/Logo"
 import { FlexWrapper } from "../../../components/FlexWrapper"
+import { Container } from "../../../components/Container"
+import { Theme } from "../../../styles/Theme"
 
 const contactsItems = ["Projects", "About", "Digital Assets"]
 
 export const Contacts = () => {
     return (
         <StyledContacts>
-            <FlexWrapper justify="space-between">
-                <div>
-                    <Logo/>
-                    <ContactsMenu menuItems={contactsItems}/>
-                </div>
-                <div>
-                    <Title>Subscribe to my emailing list</Title>
-                    <StyledForm>
-                        <Field placeholder={"Enter your email"}/>
-                        <Button>Subscribe</Button>
-                    </StyledForm>
-                    <Text>By subscribing you agree to with our Privacy Policy</Text>
-                </div>
-            </FlexWrapper>
-
+            <Container>
+                <FlexWrapper justify="space-between">
+                    <div>
+                        <Logo/>
+                        <ContactsMenu menuItems={contactsItems}/>
+                    </div>
+                    <div>
+                        <Title>Subscribe to my emailing list</Title>
+                        <StyledForm>
+                            <Field placeholder={"Enter your email"}/>
+                            <Button>Subscribe</Button>
+                        </StyledForm>
+                        <Text>By subscribing you agree to with our <a href="">Privacy Policy</a></Text>
+                    </div>
+                </FlexWrapper>
+            </Container>
         </StyledContacts>
     )
 }
 
 const StyledContacts = styled.section`
-    min-height: 50vh;
-    background-color: #c3f0e6;
+    padding: 80px 108px 64px;
+`
+
+const Title = styled.h5`
+    font-family: "Epilogue", sans-serif;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${Theme.colors.secondaryFn};
+    margin-top: 12px;
 `
 
 const StyledForm = styled.form`
@@ -38,20 +49,52 @@ const StyledForm = styled.form`
     display: flex;
     flex-direction: row;
     gap: 16px;
+    margin: 16px 0;
 `
 
 const Field = styled.input`
-    
-`
+    width: 257px;
+    border: 1px solid ${Theme.colors.secondaryFn};
+    padding: 12px;
+    height: 48px;
+    background-color: ${Theme.colors.mainFn};
+    font-family: "Epilogue", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${Theme.colors.secondaryFn};
 
-const Title = styled.h3`
-    
-`
+    &::placeholder {
+        color: ${Theme.colors.secondaryFn};
+    }
 
-const Text = styled.p`
-    
+    &:focus-visible {
+        outline: 1px solid ${Theme.colors.secondaryFn};
+    }
 `
 
 const Button = styled.button`
-    
+    border: 1px solid ${Theme.colors.secondaryFn};
+    padding: 12px 24px;
+    width: 127px;
+    height: 48px;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${Theme.colors.secondaryFn};
+`
+
+const Text = styled.p`
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 1.5;
+    color: ${Theme.colors.secondaryFn};
+    margin-bottom: 12px;
+
+    a {
+        text-decoration: underline;
+        text-decoration-skip-ink: none;
+        color: ${Theme.colors.secondaryFn};
+    }
 `
