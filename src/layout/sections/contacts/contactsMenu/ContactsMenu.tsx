86b8951ea66d632298f3da251b45979fac10356d
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-03-20 16:51:06 +0300

    Style ContactsMenu

--- src/layout/sections/contacts/contactsMenu/ContactsMenu.tsx
+++ src/layout/sections/contacts/contactsMenu/ContactsMenu.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { Theme } from "../../../../styles/Theme";
 
 export const ContactsMenu = (props: {menuItems: Array<string>}) => {
     return (
@@ -18,9 +19,14 @@ const StyledContactsMenu = styled.nav`
     ul {
         display: flex;
         gap: 32px;
+        margin-top: 20px;
     }
 `
 
 const Link = styled.a`
-    
+    font-family: "Epilogue", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${Theme.colors.secondaryFn};
 `
