068f61921f490628d8299491d16b758347941347
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-04-03 22:56:43 +0300

    Style h1

--- src/components/h1/H1.tsx
+++ src/components/h1/H1.tsx
@@ -1,12 +1,12 @@
 
 export const MainTitle = () => {
     return (
-            <svg width="100%" height="25%" xmlns="http://www.w3.org/2000/svg">
+            <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
             <defs>
-                <path id="textPath" d="M 80 45 q 150 150 300 0" />
+                <path id="textPath" d="M -30 -10 q 300 300 600 50" />
             </defs>
-            <text font-family="Epilogue, sans-serif" font-size="30px" font-weight="bold">
-                <textPath href="#textPath" startOffset="20">
+            <text font-family="Epilogue, sans-serif" font-size="50px" font-weight="500" color="#2D2C2C">
+                <textPath href="#textPath" startOffset="120">
                 Front-end Developer
                 </textPath>
             </text>
