7fffadc9394bc4b8160a6fd3e91eb7ead69a3a59
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-04-09 14:05:07 +0300

    Adaptiv main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,17 +2,17 @@ import styled from 'styled-components';
 import photo from '../../../assets/images/myPhoto.webp'
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Container } from '../../../components/Container';
-import { MainTitle } from '../../../components/h1/H1';
 import { Theme } from '../../../styles/Theme';
+import front from '../../../assets/images/front.png'
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
-                    <FlexWrapper direction={"column"} justify={"flex-start"} align={"left"}>
-                        <SectionTitle>HELLO <br/> I’M ANASTASIYA</SectionTitle>
-                        <MainTitle/>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={'wrap'}>
+                    <FlexWrapper direction={"column"} align={"left"}>
+                        <SectionTitle>HELLO <br/> I’M <br/> ANASTASIYA</SectionTitle>
+                        <TextPhoto src={front} alt="Main title"/>
                     </FlexWrapper>
                     <PhotoWrapper>
                         <Photo src={photo} alt="My photo"/>
@@ -25,8 +25,33 @@ export const Main = () => {
 
 const StyledMain = styled.section`
     min-height: 100vh;
-    display: flex;
     padding: 200px 105px 100px;
+    
+    @media ${Theme.media.mobile} {
+        padding-top: 110px !important;
+    }
+`
+
+const SectionTitle = styled.h2`
+    font-family: "Epilogue", sans-serif;
+    font-weight: 800;
+    font-size: 90px;
+    color: ${Theme.colors.primaryFn};
+    
+    @media ${Theme.media.mobile} {
+        font-size: 50px;
+    }
+`
+
+const TextPhoto = styled.img`
+    width: 80%;
+    height: 80%;
+    object-fit: cover;
+
+    @media ${Theme.media.mobile} {
+        width: 100%;
+        height: 100%;
+    }
 `
 
 const PhotoWrapper = styled.div`
@@ -35,29 +60,32 @@ const PhotoWrapper = styled.div`
     &::before {
         content: "";
         display: inline-block;
-        width: 570px;
-        height: 570px;
+        width: 500px;
+        height: 500px;
         background: #e7e7e7;
         transform: rotate(-9deg) translateY(-50%);
         position: absolute;
         top: 50%;
         transform-origin: top;
+
+        @media ${Theme.media.mobile} {
+            width: 280px;
+            height: 280px;
+            margin-top: 15px;
+        }
     }
 `
 
 const Photo = styled.img`
-    width: 570px;
-    height: 570px;
+    width: 500px;
+    height: 500px;
     object-fit: cover;
     transform: rotate(8deg);
     opacity: 0.9;
-`
-
-const SectionTitle = styled.h2`
-    font-family: "Epilogue", sans-serif;
-    font-weight: 800;
-    font-size: 90px;
-    color: ${Theme.colors.primaryFn};
-    margin-left: 30px;
-    margin-top: 20px;
+    
+    @media ${Theme.media.mobile} {
+        width: 280px;
+        height: 280px;
+        margin-top: 30px;
+    }
 `
