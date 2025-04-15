a12888b3cfd4d802f0baf3190083d6e45375ee39
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2025-04-15 18:18:18 +0300

    Refactoring main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,91 +1,23 @@
-import styled from 'styled-components';
 import photo from '../../../assets/images/myPhoto.webp'
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Container } from '../../../components/Container';
-import { Theme } from '../../../styles/Theme';
 import front from '../../../assets/images/front.png'
-import { font } from '../../../styles/Common';
+import { S } from './Main_Styles';
 
-export const Main = () => {
+export const Main: React.FC = () => {
     return (
-        <StyledMain>
+        <S.Main>
             <Container>
                 <FlexWrapper align={"center"} justify={"space-around"} wrap={'wrap'}>
-                    <FlexWrapper direction={"column"} align={"center"}>
-                        <SectionTitle>HELLO <br/> I’M <br/> ANASTASIYA</SectionTitle>
-                        <TextPhoto src={front} alt="Main title"/>
-                    </FlexWrapper>
-                    <PhotoWrapper>
-                        <Photo src={photo} alt="My photo"/>
-                    </PhotoWrapper>
+                    <S.TitleWrapper>
+                        <S.SectionTitle>HELLO <br/> I’M <br/> ANASTASIYA</S.SectionTitle>
+                        <S.TextPhoto src={front} alt="Main title"/>
+                    </S.TitleWrapper>
+                    <S.PhotoWrapper>
+                        <S.Photo src={photo} alt="My photo"/>
+                    </S.PhotoWrapper>
                 </FlexWrapper>
             </Container>
-        </StyledMain>
+        </S.Main>
     );
 };
-
-const StyledMain = styled.section`
-    min-height: 100vh;
-    padding: 150px 105px 100px;
-    
-    @media ${Theme.media.mobile} {
-        padding-top: 120px !important;
-    }
-`
-
-const SectionTitle = styled.h2`
-    ${font({family: "'Epilogue', sans-serif", weight: 800, Fmax: 90, Fmin: 50})};
-    color: ${Theme.colors.primaryFn};
-    text-align: left;
-`
-
-const TextPhoto = styled.img`
-    width: 80%;
-    height: 80%;
-    object-fit: cover;
-    margin-bottom: 40px;
-`
-
-const PhotoWrapper = styled.div`
-    position: relative;
-
-    &::before {
-        content: "";
-        display: inline-block;
-        width: 500px;
-        height: 500px;
-        background: #e7e7e7;
-        transform: rotate(-8deg) translateY(-50%);
-        position: absolute;
-        top: 50%;
-        transform-origin: top;
-
-        @media ${Theme.media.tablet} {
-            width: 400px;
-            height: 400px;
-        }
-
-        @media ${Theme.media.mobile} {
-            width: 280px;
-            height: 280px;
-        }
-    }
-`
-
-const Photo = styled.img`
-    width: 500px;
-    height: 500px;
-    object-fit: cover;
-    transform: rotate(9deg);
-    opacity: 0.9;
-    
-    @media ${Theme.media.tablet} {
-        width: 400px;
-        height: 400px;
-    }
-
-    @media ${Theme.media.mobile} {
-        width: 280px;
-        height: 280px;
-    }
-`
---
+++ src/layout/sections/main/Main_Styles.ts
@@ -0,0 +1,82 @@
+import styled from "styled-components"
+import { Theme } from "../../../styles/Theme"
+import { font } from "../../../styles/Common"
+
+const Main = styled.section`
+    min-height: 100vh;
+    padding: 150px 105px 100px;
+    
+    @media ${Theme.media.mobile} {
+        padding-top: 120px !important;
+    }
+`
+
+const TitleWrapper = styled.div`
+    max-width: 565px;
+`
+
+const SectionTitle = styled.h2`
+    ${font({family: "'Epilogue', sans-serif", weight: 800, Fmax: 90, Fmin: 50})};
+    color: ${Theme.colors.primaryFn};
+    text-align: left;
+`
+
+const TextPhoto = styled.img`
+    width: 80%;
+    height: 80%;
+    object-fit: cover;
+    margin-bottom: 40px;
+`
+
+const PhotoWrapper = styled.div`
+    position: relative;
+
+    &::before {
+        content: "";
+        display: inline-block;
+        width: 500px;
+        height: 500px;
+        background: #e7e7e7;
+        transform: rotate(-8deg) translateY(-50%);
+        position: absolute;
+        top: 50%;
+        transform-origin: top;
+
+        @media ${Theme.media.tablet} {
+            width: 400px;
+            height: 400px;
+        }
+
+        @media ${Theme.media.mobile} {
+            width: 280px;
+            height: 280px;
+        }
+    }
+`
+
+const Photo = styled.img`
+    width: 500px;
+    height: 500px;
+    object-fit: cover;
+    transform: rotate(9deg);
+    opacity: 0.9;
+    
+    @media ${Theme.media.tablet} {
+        width: 400px;
+        height: 400px;
+    }
+
+    @media ${Theme.media.mobile} {
+        width: 280px;
+        height: 280px;
+    }
+`
+
+export const S = {
+    Main,
+    TitleWrapper,
+    SectionTitle,
+    TextPhoto,
+    PhotoWrapper,
+    Photo,
+}
