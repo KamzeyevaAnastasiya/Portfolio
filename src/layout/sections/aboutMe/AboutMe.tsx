02d67fa08a3a8660c83e78e39d122bafa02b8156
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-09-22 17:29:29 +0300

    Update info about me

--- src/layout/sections/aboutMe/AboutMe.tsx
+++ src/layout/sections/aboutMe/AboutMe.tsx
@@ -15,19 +15,18 @@ export const AboutMe: React.FC = () => {
                     </S.TitleWrapper>
 
                     <S.TextWrapper>
-                        <S.Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa
-                            sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique
-                            quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet pellentesque.</S.Text>
+                        <S.Text> I’m a Frontend Developer who enjoys building modern, responsive interfaces and turning
+                            ideas into real web experiences. I work with React, TypeScript, JavaScript and a variety of
+                            modern libraries and tools while constantly exploring new technologies and improving my
+                            development skills.</S.Text>
 
-                        <S.Text>Sit turpis pretium eget maecenas. Vestibulum dolor mattis consectetur eget commodo
-                            vitae. Amet pellentesque sit pulvinar lorem mi a, euismod risus rhoncus. Elementum
-                            ullamcorper nec, habitasse vulputate. Eget dictum quis est sed egestas tellus, a lectus.
-                            Quam ullamcorper in fringilla arcu aliquet fames arcu.Lacinia eget faucibus urna, nam risus
-                            nec elementum cras porta.</S.Text>
+                        <S.Text>I love learning and exploring new technologies, especially when they give me a chance to
+                            solve problems in a different ways. Personal projects, challenging tasks, and experimenting
+                            with new ideas help me grow as a developer and keep me motivated.</S.Text>
 
-                        <S.Text>Sed elementum, sed dolor purus dolor dui. Ut dictum nulla pulvinar vulputate sit
-                            sagittis in eleifend dignissim. Natoque mauris cras molestie velit. Maecenas eget adipiscing
-                            quisque viverra lectus arcu, tincidunt ultrices pellentesque.</S.Text>
+                        <S.Text>Outside of development, I enjoy traveling, working out, reading books, and discovering
+                            new things. I believe that staying curious and constantly exploring the world helps me bring
+                            fresh ideas and a different perspective to what I create.</S.Text>
                         <S.SecondImageWrapper>
                             <S.SecondImage src={MyPhoto} alt=""/>
                         </S.SecondImageWrapper>
